6489fc6bca074567a3fc1f96291a02721acfabc9
Mahmud Mai <mahmudx00@gmail.com>
2023-05-23 07:29:25 +0100

    Feat: create endpoint for getCustomers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Box, Typography, useTheme } from '@mui/material'
 import React from 'react'
 
 const Header = ({ title, subtitle}) => {
-    const theme = useTheme
+    const theme = useTheme()
   return (
     <Box>
         <Typography variant='h2' color={theme.palette.secondary[100]} fontWeight='bold' sx={{ mb: '5px'}}>
