6a8ef1f74d8b8dac7d9e9b8c783e84a51ade2c46
Mahmud Mai <mahmudx00@gmail.com>
2023-05-24 17:32:34 +0100

    Feat: create Transactions page and route

--- src/scenes/transactions/index.jsx
+++ src/scenes/transactions/index.jsx
@@ -1,7 +1,24 @@
-import { Box } from "@mui/material";
-import React from "react";
+import { Box, useTheme } from "@mui/material";
+import React, { useState } from "react";
+import { useGetTransactionsQuery } from "state/api";
 
 const Transactions = () => {
+  const theme = useTheme();
+
+  // values to send to backend
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(20);
+  const [sort, setSort] = useState({});
+  const [search, setSearch] = useState("");
+
+  const { data, isLoading } = useGetTransactionsQuery({
+    page,
+    pageSize,
+    sort,
+    search,
+  });
+  console.log("🚀 ~ file: index.jsx:20 ~ Transactions ~ data:", data);
+
   return <Box></Box>;
 };
 
