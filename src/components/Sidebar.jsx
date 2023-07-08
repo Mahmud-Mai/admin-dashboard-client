d43df3b6d82ac6810b982de5b2d65089c6878898
Mahmud Mai <mahmudx00@gmail.com>
2023-07-08 17:49:28 +0100

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,55 +1,75 @@
-import { Box, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, useTheme } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
-import FlexBetween from './FlexBetween'
-import { ChevronLeft, ChevronRightOutlined, SettingsOutlined } from '@mui/icons-material'
-import { navItems } from 'common/Constants'
-import profileImage from '../assets/profile.jpeg'
-
+import {
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import FlexBetween from "./FlexBetween";
+import {
+  ChevronLeft,
+  ChevronRightOutlined,
+  SettingsOutlined,
+} from "@mui/icons-material";
+import { navItems } from "common/Constants";
+import profileImage from "../assets/profile.jpeg";
 
 const Sidebar = ({
-    user,
-    drawerWidth,
-    isSidebarOpen,
-    setIsSidebarOpen,
-    isNonMobile,
-  }) => {
-    const { pathname } = useLocation();
-    const [active, setActive] = useState("");
-    const navigate = useNavigate();
-    const theme = useTheme();
-  
-    useEffect(() => {
-      setActive(pathname.substring(1));
-    }, [pathname]);
-  
-  
+  user,
+  drawerWidth,
+  isSidebarOpen,
+  setIsSidebarOpen,
+  isNonMobile,
+}) => {
+  const { pathname } = useLocation();
+  const [active, setActive] = useState("");
+  const navigate = useNavigate();
+  const theme = useTheme();
+
+  useEffect(() => {
+    setActive(pathname.substring(1));
+  }, [pathname]);
 
   return (
     <Box component="nav">
-        {isSidebarOpen && (
-      <Drawer
-            open={isSidebarOpen}
-            onClose={() => setIsSidebarOpen(false)}
-            variant="persistent"
-            anchor="left"
-            sx={{
+      {isSidebarOpen && (
+        <Drawer
+          open={isSidebarOpen}
+          onClose={() => setIsSidebarOpen(false)}
+          variant="persistent"
+          anchor="left"
+          sx={{
             width: drawerWidth,
             "& .MuiDrawer-paper": {
-                color: theme.palette.secondary[200],
-                backgroundColor: theme.palette.background.alt,
-                boxSixing: "border-box",
-                borderWidth: isNonMobile ? 0 : "2px",
-                width: drawerWidth,
+              color: theme.palette.secondary[200],
+              backgroundColor: theme.palette.background.alt,
+              boxSixing: "border-box",
+              borderWidth: isNonMobile ? 0 : "2px",
+              width: drawerWidth,
             },
-            }}
-            >
+          }}
+        >
           <Box width="100%">
             <Box m="1.5rem 2rem 2rem 3rem">
               <FlexBetween color={theme.palette.secondary.main}>
-                <Box display="flex" alignItems="center" gap="0.5rem">
-                  <Typography variant="h4" fontWeight="bold">
-                    MYFIRM
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  gap="0.5rem"
+                >
+                  <Typography
+                    variant="h4"
+                    fontWeight="bold"
+                  >
+                    BILLIONAIRE BOYS INC.
                   </Typography>
                 </Box>
                 {!isNonMobile && (
@@ -63,7 +83,10 @@ const Sidebar = ({
               {navItems.map(({ text, icon }) => {
                 if (!icon) {
                   return (
-                    <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
+                    <Typography
+                      key={text}
+                      sx={{ m: "2.25rem 0 1rem 3rem" }}
+                    >
                       {text}
                     </Typography>
                   );
@@ -71,7 +94,10 @@ const Sidebar = ({
                 const lcText = text.toLowerCase();
 
                 return (
-                  <ListItem key={text} disablePadding>
+                  <ListItem
+                    key={text}
+                    disablePadding
+                  >
                     <ListItemButton
                       onClick={() => {
                         navigate(`/${lcText}`);
@@ -110,9 +136,16 @@ const Sidebar = ({
             </List>
           </Box>
 
-          <Box position="absolute" bottom="2rem">
+          <Box
+            position="absolute"
+            bottom="2rem"
+          >
             <Divider />
-            <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+            <FlexBetween
+              textTransform="none"
+              gap="1rem"
+              m="1.5rem 2rem 0 3rem"
+            >
               <Box
                 component="img"
                 alt="profile"
@@ -146,9 +179,9 @@ const Sidebar = ({
             </FlexBetween>
           </Box>
         </Drawer>
-    )}
+      )}
     </Box>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
